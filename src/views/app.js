0eccfbc188ebe3b492bb61eb01fb4d056815eeea
“ffeller” <flaris.feller@gmail.com>
2025-09-29 08:41:35 -0300

    Criar endpoint integrantes

--- src/views/app.js
+++ src/views/app.js
@@ -1,3 +1,12 @@
 app.get('/', (req, res) => {
 res.sendFile('views/index.html', { root: __dirname })
 })
+
+data = {'integrantes':[
+  {'nome':'fulano da silva'},
+  {'nome':'ciclano sauro'}
+]}
+
+app.get('/integrantes', (req, res) => {
+  res.json(data)
+})
