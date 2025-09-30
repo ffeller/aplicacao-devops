13e3a30410165ee2b38c3e931883a9f652c62610
“ffeller” <flaris.feller@gmail.com>
2025-09-30 00:57:42 -0300

    Corrigir bugs ao iniciar o servidor

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
+express = require('express')
+app = express()
+port = 3000
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
+
 app.get('/', (req, res) => {
 res.sendFile('views/index.html', { root: __dirname })
 })
 
 data = {'integrantes':[
   {'nome':'fulano da silva'},
-  {'nome':'ciclano sauro'}
+  {'nome':'ciclano sauro'},
+  {'nome':'Flaris Feller'}
 ]}
 
 app.get('/integrantes', (req, res) => {
